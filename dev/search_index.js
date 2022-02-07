628e6c8322889106ccde25d5e7087fc348eaddb6
Documenter.jl <documenter@juliadocs.github.io>
2022-02-07 11:56:48 +0000

    build based on 7e30903

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Copulas","category":"page"},{"location":"#Copulas","page":"Home","title":"Copulas","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Copulas.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Copulas]","category":"page"}]
+}
